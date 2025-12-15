53817421a9e9d1929496ec72c8186ec745627b56
daydedaa <daydedaa@gmail.com>
2025-12-15 17:09:10 +0700

    third commit non-comment

--- Tutorial-1-Multi_Dimension-Array/Test.java
+++ Tutorial-1-Multi_Dimension-Array/Test.java
@@ -9,10 +9,7 @@ public class Test {
         final int COLS = 2;
         double myList[][] = new double[MAX_ROWS][COLS];
 
-        String filename = "./t1.csv";
-        if(args.length > 0){
-            filename = args[0];
-        }
+        String filename = args[0];
         File f = new File(filename);
         Scanner myScan = new Scanner(f);
         myScan.useDelimiter("[,\\s]+");
@@ -31,7 +28,7 @@ public class Test {
         int rowsRead = count / COLS;
         for (int i = 0; i < rowsRead; i++) {
             for (int j = 0; j < COLS; j++) {
-                sum += myList[i][j]; 
+                sum += myList[i][j];
             }
         }
         System.out.println("Sum for both columns A+B : " + sum);
@@ -39,7 +36,7 @@ public class Test {
 
         double[] sums = new double[rowsRead];
 
-        for(int i = 0; i < rowsRead; i++){
+        for (int i = 0; i < rowsRead; i++) {
             double sumOfRows = 0;
             double a = myList[i][0];
             double b = myList[i][1];
@@ -52,13 +49,13 @@ public class Test {
         Arrays.sort(sums);
 
         System.out.println("Top 5 sums (highest): ");
-        for (int i = rowsRead - 1; i >= Math.max(0, rowsRead - 5); i--){
+        for (int i = rowsRead - 1; i >= Math.max(0, rowsRead - 5); i--) {
             System.out.println(sums[i]);
         }
         System.out.println();
 
         System.out.println("Bottom 5 sums (lowest): ");
-        for (int i = 0; i < 5 && i < rowsRead; i++){
+        for (int i = 0; i < 5 && i < rowsRead; i++) {
             System.out.println(sums[i]);
         }
         myScan.close();
