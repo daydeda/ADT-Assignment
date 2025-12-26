f9f370589e266d8914ee4ebbfa41f243ec2a8ab9
daydedaa <daydedaa@gmail.com>
2025-12-26 16:50:56 +0700

    Tutorial2 complete

--- Tutorial-2/studentClass.java
+++ Tutorial-2/studentClass.java
@@ -2,10 +2,22 @@ class studentClass {
     private int sid;
     private String fname;
     private String lname;
-    
-    public studentClass(int sid, String fname, String lname){
+
+    public studentClass(int sid, String fname, String lname) {
         this.sid = sid;
         this.fname = fname;
         this.lname = lname;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
     }
 }
--- Tutorial-2/studentMain.java
+++ Tutorial-2/studentMain.java
@@ -1,11 +1,58 @@
-import java.util.Scanner;
 import java.io.*;
+import java.util.*;
 
 public static void main(String[] args) throws FileNotFoundException {
-    Scanner fileInput = new Scanner(new File("students.txt"));
-    while (fileInput.hasNextLine()) {
-        String line = fileInput.nextLine();
-        System.out.println(line);
+    // 1. Check arguments
+    if (args.length != 2) {
+        System.out.println("Usage: $>java studentMain -[n/f/l] [filename]");
+        return;
     }
-    fileInput.close();
+    String option = args[0];
+    String filename = args[1];
+
+    // 2. Read student data from file
+    List<studentClass> students = new ArrayList<>();
+    Scanner fileScanner = new Scanner(new File(filename));
+
+    while (fileScanner.hasNextLine()) {
+        String line = fileScanner.nextLine();
+        String[] data = line.split(",");
+
+        if (data.length > 3 && isNumber(data[1])) {
+            int sid = Integer.parseInt(data[1].trim()); // Column 2: is ID
+            String fname = data[2].trim(); // Column 3: is First Name
+            String lname = data[3].trim(); // Column 4: is Last Name
+            students.add(new studentClass(sid, fname, lname));
+        }
+    }
+
+    // 3. Sort based on the option
+    if (option.equals("-n")) {
+        // Sort by student ID
+        System.out.println("Sorting by student ID: ");
+        students.sort((s1, s2) -> s1.getSid() - s2.getSid());
+    } else if (option.equals("-f")) {
+        // Sort by first name
+        System.out.println("Sorting by first name: ");
+        students.sort((s1, s2) -> s1.getFname().compareTo(s2.getFname()));
+    } else if (option.equals("-l")) {
+        // Sort by last name
+        System.out.println("Sorting by last name: ");
+        students.sort((s1, s2) -> s1.getLname().compareTo(s2.getLname()));
+    }
+
+    // 4. Print sorted student data
+    for (studentClass student : students) {
+        System.out.println(student.getSid() + ", " + student.getFname() + ", " + student.getLname());
+    }
+    fileScanner.close();
+}
+
+public static boolean isNumber(String str) {
+    try {
+        Integer.parseInt(str);
+        return true;
+    } catch (NumberFormatException e) {
+        return false;
+    }
 }
